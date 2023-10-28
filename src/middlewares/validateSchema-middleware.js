48b928ea2f2b65f0a123a6398a8a2258565aba03
Vanessa Furquim <vanessafurquim.contato@gmail.com>
2023-10-27 23:42:44 -0300

    REQUIREMENT: throw error method on route POST /cities - implemented and tested; REQUIREMENT: errorHandler middleware - implemented and tested.

--- src/middlewares/validateSchema-middleware.js
+++ src/middlewares/validateSchema-middleware.js
@@ -5,8 +5,8 @@ export function validateSchema(schema) {
         if (validation.error) {
             const errors = validation.error.details.map(detail => detail.message)
             console.log(errors)
-            return response.status(422).send(errors)
-            // throw { type: "UNPROCESSABLE ENTITY", message: error.message }
+            
+            throw { type: "UNPROCESSABLE ENTITY", message: validation.error.message }
         }
 
         next()
